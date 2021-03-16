6afe8cc62e29407673243dd7190dd7b2f58236a2
Love Gruvberg <Grooveberg@Loves-MacBook-Pro.local>
2021-03-16 14:43:00 +0100

    Finished da project

--- src/main/java/example_maven2/exmvn2/Main.java
+++ src/main/java/example_maven2/exmvn2/Main.java
@@ -3,8 +3,27 @@ package example_maven2.exmvn2;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		MyRandomMethods2 mrm = new MyRandomMethods2();
 
+		for (int i = 0; i < 10; i++) {
+			System.out.println(mrm.randomBetween1And10()+", ");
+		}
+		System.out.println("");
+		for (int i = 0; i < 20; i++) {
+			System.out.println(mrm.randomBetween11And20()+", ");
+		}
+		System.out.println("");
+		for (int i = 0; i < 30; i++) {
+			System.out.println(mrm.randomBetween21And30()+", ");
+		}
+		System.out.println("");
+		for (int i = 0; i < 40; i++) {
+			System.out.println(mrm.randomBetween31And40()+", ");
+		}
+		System.out.println("");
+		for (int i = 0; i < 50; i++) {
+			System.out.println(mrm.randomBetween41And50()+", ");
+		}
 	}
 
 }
